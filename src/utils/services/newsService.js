203d37dd481dee1c25716d9f91d0aa050a6c99f1
Pabearyoga <pabearyoga@gmail.com>
2023-11-20 20:24:54 +0200

    add newsServices

---
+++ src/utils/services/newsService.js
@@ -0,0 +1,20 @@
+const token = 'f3a6a56cecaa43a297ce2e610ebe343a';
+
+export const getAllNews = async () => {
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${token}`
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch: ${response.status} - ${response.statusText}`
+      );
+    }
+
+    const newsList = await response.json();
+    return newsList;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
