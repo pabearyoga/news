65d304c0f3e4af3e8253d033f93a48191cb9b505
Pabearyoga <pabearyoga@gmail.com>
2023-11-20 20:23:57 +0200

    add pages Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+// import FilterSelect from '../../components/FilterSelect/FilterSelect';
+import Search from '../../components/Search/Search';
+import Title from '../../components/Title/Title';
+import { getAllNews } from '../../utils/services/newsService';
+import css from './Home.module.css';
+
+
 const Home = () => {
+    const [news, setNews] = useState([]);
+
+
+    useEffect(() => {
+        const fetchNews = async () => {
+            try {
+                const newsList = await getAllNews();
+                setNews(newsList.articles);
+            } catch (error) {
+                console.error(error.message);
+            }
+        }
+            fetchNews();
+    }, []); 
+    
+    console.log(news)
+    
     return (
-        <div>Home</div>
+        <div className={css.home}>
+            <div className={css.wrapper}>
+                <Title>News Top Headlines</Title>
+                <Search />
+                {/* <FilterSelect
+                    name='category'
+                    selectValue={category}
+                    handleSelectClick={categorySelectClick}
+                    Icon={CiFilter}
+                    showOption={showCategoryOption}
+                    optionList={categoryList}
+                    handleOptionSelect={handleOptionSelect}
+                ></FilterSelect> */}
+            </div>
+
+        </div>
     )
 };
 
