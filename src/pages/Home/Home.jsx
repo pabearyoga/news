8d7c479561cd92ba9e18dbe7ddabd28c1b83f553
Pabearyoga <pabearyoga@gmail.com>
2023-11-21 22:36:31 +0200

    fix path

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 // import FilterSelect from '../../components/FilterSelect/FilterSelect';
 import Search from '../../components/Search/Search';
 import Title from '../../components/Title/Title';
-import { getAllNews } from '/src/utils/services/newsService.js';
+import { getAllNews } from '../../utils/services/newsService.js';
 import css from './Home.module.css';
 import NewsList from '../../components/NewsList/NewsList';
 import {FilterBtn} from '../../components/FilterBtn/FilterBtn'
@@ -28,6 +28,8 @@ const Home = () => {
         }
             fetchNews();
     }, []); 
+
+    console.log(news)
         
     return (
         <div className={css.home}>
