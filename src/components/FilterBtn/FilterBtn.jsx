53f407e06ce02314f029fcd576e4702ce62373b8
Pabearyoga <pabearyoga@gmail.com>
2023-11-22 13:29:46 +0200

    add mui to FilterBtn

--- src/components/FilterBtn/FilterBtn.jsx
+++ src/components/FilterBtn/FilterBtn.jsx
@@ -1,9 +1,18 @@
-import css from './FilterBtn.module.css';
-import {CiFilter} from 'react-icons/ci'
+import React from 'react';
+import Button from '@mui/material/Button';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
 
-
-export const FilterBtn = ({handleSelectClick}) => {
-    return (
-        <button onClick={handleSelectClick} className={css.btn}><CiFilter size={24} color='#888' />Filters</button>
-    )
+const FilterBtn = ({ handleSelectClick }) => {
+  return (
+    <Button
+        variant="contained"
+        style={{ backgroundColor: 'var(--accent)', boxShadow: 'none',height: '56px', padding: '0 30px', color: 'var(--text)',  textTransform: 'capitalize'}}
+        onClick={handleSelectClick}
+    >
+      <FilterAltIcon sx={{ fontSize: 24, color: '#888', marginRight: 1 }} />
+      Filters
+    </Button>
+  );
 };
+
+export default FilterBtn;
