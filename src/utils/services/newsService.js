f9aae77202e329e668a92dbe578fcf97581ccd22
Pabearyoga <pabearyoga@gmail.com>
2023-11-23 19:08:31 +0200

    add axios

--- src/utils/services/newsService.js
+++ src/utils/services/newsService.js
@@ -1,5 +1,5 @@
 // newsService.js
-
+import axios from 'axios';
 const token = 'f3a6a56cecaa43a297ce2e610ebe343a';
 
 export const getAllNews = async () => {
@@ -20,6 +20,52 @@ export const getAllNews = async () => {
     console.error(error.message);
   }
 };
+
+// export const getFilterNews = async ({ search, country, content }) => {
+//   try {
+//     const baseUrl = 'https://newsapi.org/v2/top-headlines';
+//     const apiKey = `apiKey=${token}`;
+//     const pageSize = 'pageSize=100';
+//     const page = 'page=1';
+
+//     const queryCountryParams = [];
+//     const queryParams = [];
+
+//     if (country) {
+//       queryCountryParams.push(`country=${country}`);
+//     }
+//     if (!country) {
+//       queryCountryParams.push(`country=us`);
+//     }
+//     if (search) {
+//       queryParams.push(`q=${search}`);
+//     }
+//     if (content && content !== 'Selected') {
+//       queryParams.push(`category=${content}`);
+//     }
+
+//     const queryStringCountry =
+//       queryCountryParams.length > 0 ? `?${queryCountryParams.join('&')}` : '';
+
+//     const queryString =
+//       queryParams.length > 0 ? `${queryParams.join('&')}` : '';
+
+//     const response = await fetch(
+//       `${baseUrl}${queryStringCountry}&${apiKey}&${pageSize}&${page}&${queryString}`
+//     );
+
+//     if (!response.ok) {
+//       throw new Error(
+//         `Failed to fetch: ${response.status} - ${response.statusText}`
+//       );
+//     }
+
+//     const newsList = await response.json();
+//     return newsList;
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// };
 
 export const getFilterNews = async ({ search, country, content }) => {
   try {
@@ -50,19 +96,13 @@ export const getFilterNews = async ({ search, country, content }) => {
     const queryString =
       queryParams.length > 0 ? `${queryParams.join('&')}` : '';
 
-    const response = await fetch(
+    const response = await axios.get(
       `${baseUrl}${queryStringCountry}&${apiKey}&${pageSize}&${page}&${queryString}`
     );
 
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch: ${response.status} - ${response.statusText}`
-      );
-    }
-
-    const newsList = await response.json();
-    return newsList;
+    return response.data;
   } catch (error) {
-    console.error(error.message);
+    console.error('Error in getFilterNews:', error.message);
+    throw new Error('Failed to fetch filtered news.');
   }
 };
